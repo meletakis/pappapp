635ec703e363988cbd9bfe2b50805301041dcc5d
mele25 <mele25@hotmail.com>
2017-04-22 12:02:29 +0300

    rename auth model/ctrl to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'static#index'
 
+  get '/register' => 'users#new'
+  post '/register' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/migrate/20170422085214_create_users.rb
+++ db/migrate/20170422085214_create_users.rb
@@ -1,6 +1,6 @@
-class CreateAuths < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :auths do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password_digest
